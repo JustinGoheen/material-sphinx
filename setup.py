a6eba8429a915c125693fb4aec84e3449b3cbcc7
Justin G <26209687+JustinGoheen@users.noreply.github.com>
2022-04-21 22:36:34 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from setuptools import find_packages
+from pathlib import Path
+
+this_file = Path(__file__).resolve()
+readme = this_file.parent / "README.md"
+
+
+setup(
+    name="easy-sphinx",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["markdown", "sphinx", "black"],
+    package_data={"": ["README.md"]},
+    author="Justin Goheen",
+    author_email="j.goheen@protonmail.com",
+    description="A simple interface to Sphinx autodoc.",
+    long_description=readme.read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/keras-team/keras-autodoc",
+    license="Apache License 2.0",
+    extras_require={"tests": ["pytest", "pytest-pep8"]},
+    classifiers=[
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Utilities",
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
---
+++ tests/test_integration.py
@@ -0,0 +1,26 @@
+from keras import Model
+from keras_tuner import HyperParameters
+from keras_autodoc.get_signatures import get_function_signature, get_signature_end
+
+
+def test_signature():
+    expected = (
+        "Model.compile(\n"
+        '    optimizer="rmsprop",\n'
+        "    loss=None,\n"
+        "    metrics=None,\n"
+        "    loss_weights=None,\n"
+        "    weighted_metrics=None,\n"
+        "    run_eagerly=None,\n"
+        "    steps_per_execution=None,\n"
+        "    **kwargs\n"
+        ")"
+    )
+    computed = get_function_signature(Model.compile)
+    assert computed == expected
+
+
+def test_wrapping_signature():
+    expected = "(parent_name, parent_values)"
+    computed = get_signature_end(HyperParameters.conditional_scope)
+    assert computed == expected
